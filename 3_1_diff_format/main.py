e5976518a52f90463b6190071b53e87179e333c1
Matvey Malkov <work.malkov.matvey@gmail.com>
2019-07-08 18:54:46 +0500

    Rework homework 3_1. Fix output

--- 3_1_diff_format/main.py
+++ 3_1_diff_format/main.py
@@ -10,18 +10,19 @@ def check_in_strict(strict, word) -> None:
     :param word: слово
     :return: None
     """
-    if word in strict.keys():
-        strict.update([(word, strict[word] + 1)])
-    else:
-        strict[word] = 1
-    return
+    if len(word) > 6:
+        if word in strict.keys():
+            strict.update([(word, strict[word] + 1)])
+        else:
+            strict[word] = 1
+        return
 
 
 def json_top_words() -> str:
     """
     Работает с json форматом.
     Возвращает строку, которая содержит
-    10 самых часто используемых слов в статьях через пробел
+    10 самых часто используемых слов в статьях c новой строки.
     :return: str
     """
     json_words = {}
@@ -31,14 +32,14 @@ def json_top_words() -> str:
         for word in item['description'].lower().split():
             check_in_strict(json_words, word)
     json_words = sorted(json_words.items(), key=lambda kv: kv[1], reverse=True)
-    return ' '.join([x[0] for x in json_words[:11]])
+    return '\n'.join([f'{x[0]}: {x[1]}' for x in json_words[:11]])
 
 
 def xml_top_words() -> str:
     """
     Работает с xml форматом.
     Возвращает строку, которая содержит
-    10 самых часто используемых слов в статьях через пробел.
+    10 самых часто используемых слов в статьях c новой строки.
     :return: str
     """
     xml_words = {}
@@ -47,9 +48,10 @@ def xml_top_words() -> str:
         for word in item.find('description').text.lower().split():
             check_in_strict(xml_words, word)
     xml_words = sorted(xml_words.items(), key=lambda kv: kv[1], reverse=True)
-    return ' '.join([x[0] for x in xml_words[:11]])
+    return '\n'.join([f'{x[0]}: {x[1]}' for x in xml_words[:11]])
 
 
 if __name__ == '__main__':
-    print("Слова из json'а:", json_top_words())
-    print("Слова из xml'я:", xml_top_words())
+    print(f"-------------Слова из json'а:\n{json_top_words()}")
+    print(f"-------------Слова из xml'я:\n{xml_top_words()}")
+
--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Netology's homework storage
+## Simple python
+#### Introducing in python
 1. [Домашка про классы (2.2)](https://github.com/guillotine666/netology_homeworks/tree/master/2_2_classes)
 1. [Домашка про исключения (2.3)](https://github.com/guillotine666/netology_homeworks/tree/master/2_3_exceptions)
+#### Work with file
 1. [Домашка про открытие и чтение файла, запись в файл (2.4)](https://github.com/guillotine666/netology_homeworks/tree/master/2_4_files)
 1. [Домашка про менеджер контекста (2.5)](https://github.com/guillotine666/netology_homeworks/tree/master/2_5_context_manager)
 1. [Домашка про разные форматы (3.1)](https://github.com/guillotine666/netology_homeworks/tree/master/3_1_diff_format)
+#### Work with API
 1. [Домашка про request и http-запросы (3.2)](https://github.com/guillotine666/netology_homeworks/tree/master/3_2_requests_http)
