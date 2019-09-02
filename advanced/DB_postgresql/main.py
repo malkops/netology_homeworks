2798d9f23483cc950fb5c5be32702d674fef33c0
Matvey Malkov <work.malkov.matvey@gmail.com>
2019-09-02 12:37:19 +0500

    refactoring postgresql homework

--- advanced/DB_postgresql/main.py
+++ advanced/DB_postgresql/main.py
@@ -3,60 +3,56 @@ import datetime
 import psycopg2
 
 
-def create_db():
-    global cur
-    global conn
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS Course (
-    id serial PRIMARY KEY NOT NULL,
-    name varchar(100) NOT NULL);
-    
-    CREATE TABLE IF NOT EXISTS Student (
-    id serial PRIMARY KEY NOT NULL,
-    name varchar(100) NOT NULL,
-    gpa numeric(10, 2),
-    birth timestamp with time zone,
-    course_id INTEGER REFERENCES COURSE(id));
-    """)
+class WorkWithPostgre:
+    def __init__(self, dbname: str, username: str, pwd=''):
+        self.dbname = dbname
+        self.user = username
+        self.pwd = pwd
+        self.conn, self.cur = self.get_connection()
 
+    def get_connection(self):
+        conn = psycopg2.connect(dbname=self.dbname, user=self.user, password=self.pwd)
+        conn.autocommit = True
+        cur = conn.cursor()
 
-def get_student(student_id):
-    global cur
-    global conn
-    cur.execute("""SELECT * FROM Student
-    WHERE id = (%s)""", (student_id,))
-    cur.fetchall()
+        return conn, cur
 
+    def create_db(self) -> None:
+        self.cur.execute("""
+        CREATE TABLE IF NOT EXISTS Course (
+        id serial PRIMARY KEY NOT NULL,
+        name varchar(100) NOT NULL);
+        
+        CREATE TABLE IF NOT EXISTS Student (
+        id serial PRIMARY KEY NOT NULL,
+        name varchar(100) NOT NULL,
+        gpa numeric(10, 2),
+        birth timestamp with time zone,
+        course_id INTEGER REFERENCES COURSE(id));
+        """)
 
-def get_students(course_id):
-    global cur
-    global conn
-    cur.execute("""SELECT * FROM Student
-    WHERE course_id = (%s);""", (course_id,))
-    return cur.fetchall()
+    def get_student(self, student_id: int) -> dict:
+        self.cur.execute("""SELECT * FROM Student
+        WHERE id = (%s)""", (student_id,))
+        return self.cur.fetchall()
 
+    def get_students(self, course_id: int) -> dict:
+        self.cur.execute("""SELECT * FROM Student
+        WHERE course_id = (%s);""", (course_id,))
+        return self.cur.fetchall()
 
-def add_student(student, course_id=None):
-    global cur
-    global conn
-    sql = """INSERT INTO student
-    (name, gpa, birth, course_id) values (%s, %s, %s, %s);"""
-    cur.execute(sql, (*student, course_id))
+    def add_student(self, student: tuple, course_id=None) -> None:
+        sql = """INSERT INTO student
+        (name, gpa, birth, course_id) values (%s, %s, %s, %s);"""
+        self.cur.execute(sql, (*student, course_id))
 
-
-def add_students(course_id, students):
-    global cur
-    global conn
-    for student in students:
-        add_student(student, course_id=course_id)
+    def add_students(self, course_id: int, students: tuple) -> None:
+        for student in students:
+            self.add_student(student, course_id=course_id)
 
 
 if __name__ == '__main__':
-    with psycopg2.connect("dbname=guillotine_db user=guillotine") as conn:
-        with conn.cursor() as cur:
-            now = datetime.datetime.now()
-            long_ago = now - datetime.timedelta()
-            # print(get_student(1))
-            # add_student(('Андрей', 5, long_ago, 1))
-            # add_students(1, (('Алина', 4, long_ago), ('Матвей', 3, long_ago)))
-            print(get_students(1))
+    connection_to_db = WorkWithPostgre(dbname='guillotine_db', username='guillotine')
+    now = datetime.datetime.now()
+    long_ago = now - datetime.timedelta()
+    print(connection_to_db.get_student(5))
